b15ec7b824d5c6430566ef3f766da3b32bbec120
xVanTuring <xVanTuring@foxmail.com>
2021-12-10 16:10:37 +0800

    更新打包任务

--- src/Job/JobTemplates.ts
+++ src/Job/JobTemplates.ts
@@ -9,6 +9,7 @@ export const androidHotUpdateTask = [
   makeJob(JobType.PACK_GEN_ANDROID_HOT_UPDATE, false, []),
   makeJob(JobType.PACK_HOT_UPDATE, false, ["android"]),
 ];
+
 export const androidCompileTask = [
   makeJob(JobType.GIT_RESET_LOCAL, false, []),
   makeJob(JobType.GIT_CHECKOUT, false, ["develop"]),
@@ -16,6 +17,7 @@ export const androidCompileTask = [
   makeJob(JobType.BUILD_SET_ENV_VERSION, false, ["dev", "1.1.3"]),
   makeJob(JobType.BUILD_PROJECT, false, ["android"]),
   makeJob(JobType.PACK_GEN_ANDROID_HOT_UPDATE, false, []),
+  makeJob(JobType.BUILD_PROJECT, false, ["android"]),
   makeJob(JobType.PACK_COMPILE_BUNDLE, false, ["android", "dev"]),
 ];
 
@@ -28,12 +30,14 @@ export const iosHotUpdateTask = [
   makeJob(JobType.PACK_GEN_ANDROID_HOT_UPDATE, false, []),
   makeJob(JobType.PACK_HOT_UPDATE, false, ["ios"]),
 ];
-export const iosTestCompileTask = [
+
+export const iosCompileTask = [
   makeJob(JobType.GIT_RESET_LOCAL, false, []),
   makeJob(JobType.GIT_CHECKOUT, false, ["develop"]),
   makeJob(JobType.GIT_PULL, false, []),
   makeJob(JobType.BUILD_SET_ENV_VERSION, false, ["test", "1.1.3"]),
   makeJob(JobType.BUILD_PROJECT, false, ["ios"]),
   makeJob(JobType.PACK_GEN_ANDROID_HOT_UPDATE, false, []),
+  makeJob(JobType.BUILD_PROJECT, false, ["ios"]),
   makeJob(JobType.PACK_COMPILE_BUNDLE, false, ["ios", "test"]),
 ];
